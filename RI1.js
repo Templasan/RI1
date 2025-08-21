22a81328300b5ef723450e6439b5010bd8749de0
Templasan <163298566+Templasan@users.noreply.github.com>
2025-08-20 22:42:47 -0300

    Update RI1.js

--- RI1.js
+++ RI1.js
@@ -7,13 +7,15 @@ const personagem1 = {
     forca: 70,
     resistencia: 65,
     descricao: function() {
-        return "Nome do personagem: " + this.nome + "\n"
-        + "Codinome do personagem: " + this.codinome + "\n"
-        + "Arma principal: " + this.armaPrincipal + "\n"
-        + "Arma secundaria: " + this.armaSecundaria + "\n"
-        + "Nível de força: " + this.forca + "\n"
-        + "Nível de velocidade: " + this.velocidade + "\n"
-        + "Nível de resistência: " + this.resistencia + "\n";
+        return `
+    - Nome: ${this.nome}
+    - Codinome: ${this.codinome}
+    - Arma Principal: ${this.armaPrincipal}
+    - Arma Secundária: ${this.armaSecundaria}
+    - Força: ${this.forca}
+    - Velocidade: ${this.velocidade}
+    - Resistência: ${this.resistencia}
+        `;
     }
 }
 
@@ -26,13 +28,15 @@ const personagem2 = {
     forca: 80,
     resistencia: 90,
     descricao: function() {
-        return "Nome do personagem: " + this.nome + "\n"
-        + "Codinome do personagem: " + this.codinome + "\n"
-        + "Arma principal: " + this.armaPrincipal + "\n"
-        + "Arma secundaria: " + this.armaSecundaria + "\n"
-        + "Nível de força: " + this.forca + "\n"
-        + "Nível de velocidade: " + this.velocidade + "\n"
-        + "Nível de resistência: " + this.resistencia + "\n";
+        return `
+    - Nome: ${this.nome}
+    - Codinome: ${this.codinome}
+    - Arma Principal: ${this.armaPrincipal}
+    - Arma Secundária: ${this.armaSecundaria}
+    - Força: ${this.forca}
+    - Velocidade: ${this.velocidade}
+    - Resistência: ${this.resistencia}
+        `;
     }
 }
 
@@ -45,13 +49,15 @@ const personagem3 = {
     forca: 100,
     resistencia: 100,
     descricao: function() {
-        return "Nome do personagem: " + this.nome + "\n"
-        + "Codinome do personagem: " + this.codinome + "\n"
-        + "Arma principal: " + this.armaPrincipal + "\n"
-        + "Arma secundaria: " + this.armaSecundaria + "\n"
-        + "Nível de força: " + this.forca + "\n"
-        + "Nível de velocidade: " + this.velocidade + "\n"
-        + "Nível de resistência: " + this.resistencia + "\n";
+        return `
+    - Nome: ${this.nome}
+    - Codinome: ${this.codinome}
+    - Arma Principal: ${this.armaPrincipal}
+    - Arma Secundária: ${this.armaSecundaria}
+    - Força: ${this.forca}
+    - Velocidade: ${this.velocidade}
+    - Resistência: ${this.resistencia}
+        `;
     }
 }
 
@@ -64,13 +70,15 @@ const personagem4 = {
     forca: 50,
     resistencia: 60,
     descricao: function() {
-        return "Nome do personagem: " + this.nome + "\n"
-        + "Codinome do personagem: " + this.codinome + "\n"
-        + "Arma principal: " + this.armaPrincipal + "\n"
-        + "Arma secundaria: " + this.armaSecundaria + "\n"
-        + "Nível de força: " + this.forca + "\n"
-        + "Nível de velocidade: " + this.velocidade + "\n"
-        + "Nível de resistência: " + this.resistencia + "\n";
+        return `
+    - Nome: ${this.nome}
+    - Codinome: ${this.codinome}
+    - Arma Principal: ${this.armaPrincipal}
+    - Arma Secundária: ${this.armaSecundaria}
+    - Força: ${this.forca}
+    - Velocidade: ${this.velocidade}
+    - Resistência: ${this.resistencia}
+        `;
     }
 }
 
@@ -83,13 +91,15 @@ const personagem5 = {
     forca: 100,
     resistencia: 100,
     descricao: function() {
-        return "Nome do personagem: " + this.nome + "\n"
-        + "Codinome do personagem: " + this.codinome + "\n"
-        + "Arma principal: " + this.armaPrincipal + "\n"
-        + "Arma secundaria: " + this.armaSecundaria + "\n"
-        + "Nível de força: " + this.forca + "\n"
-        + "Nível de velocidade: " + this.velocidade + "\n"
-        + "Nível de resistência: " + this.resistencia + "\n";
+        return `
+    - Nome: ${this.nome}
+    - Codinome: ${this.codinome}
+    - Arma Principal: ${this.armaPrincipal}
+    - Arma Secundária: ${this.armaSecundaria}
+    - Força: ${this.forca}
+    - Velocidade: ${this.velocidade}
+    - Resistência: ${this.resistencia}
+        `;
     }
 }
 
@@ -102,13 +112,15 @@ const personagem6 = {
     forca: 60,
     resistencia: 70,
     descricao: function() {
-        return "Nome do personagem: " + this.nome + "\n"
-        + "Codinome do personagem: " + this.codinome + "\n"
-        + "Arma principal: " + this.armaPrincipal + "\n"
-        + "Arma secundaria: " + this.armaSecundaria + "\n"
-        + "Nível de força: " + this.forca + "\n"
-        + "Nível de velocidade: " + this.velocidade + "\n"
-        + "Nível de resistência: " + this.resistencia + "\n";
+        return `
+    - Nome: ${this.nome}
+    - Codinome: ${this.codinome}
+    - Arma Principal: ${this.armaPrincipal}
+    - Arma Secundária: ${this.armaSecundaria}
+    - Força: ${this.forca}
+    - Velocidade: ${this.velocidade}
+    - Resistência: ${this.resistencia}
+        `;
     }
 }
 
@@ -121,13 +133,15 @@ const personagem7 = {
     forca: 100,
     resistencia: 100,
     descricao: function() {
-        return "Nome do personagem: " + this.nome + "\n"
-        + "Codinome do personagem: " + this.codinome + "\n"
-        + "Arma principal: " + this.armaPrincipal + "\n"
-        + "Arma secundaria: " + this.armaSecundaria + "\n"
-        + "Nível de força: " + this.forca + "\n"
-        + "Nível de velocidade: " + this.velocidade + "\n"
-        + "Nível de resistência: " + this.resistencia + "\n";
+        return `
+    - Nome: ${this.nome}
+    - Codinome: ${this.codinome}
+    - Arma Principal: ${this.armaPrincipal}
+    - Arma Secundária: ${this.armaSecundaria}
+    - Força: ${this.forca}
+    - Velocidade: ${this.velocidade}
+    - Resistência: ${this.resistencia}
+        `;
     }
 }
 
@@ -140,13 +154,15 @@ const personagem8 = {
     forca: 100,
     resistencia: 100,
     descricao: function() {
-        return "Nome do personagem: " + this.nome + "\n"
-        + "Codinome do personagem: " + this.codinome + "\n"
-        + "Arma principal: " + this.armaPrincipal + "\n"
-        + "Arma secundária: " + this.armaSecundaria + "\n"
-        + "Nível de força: " + this.forca + "\n"
-        + "Nível de velocidade: " + this.velocidade + "\n"
-        + "Nível de resistência: " + this.resistencia + "\n";
+        return `
+    - Nome: ${this.nome}
+    - Codinome: ${this.codinome}
+    - Arma Principal: ${this.armaPrincipal}
+    - Arma Secundária: ${this.armaSecundaria}
+    - Força: ${this.forca}
+    - Velocidade: ${this.velocidade}
+    - Resistência: ${this.resistencia}
+        `;
     }
 }
 
@@ -166,7 +182,7 @@ function determinarVencedor(personagens) {
     let vencedor = null;
     let maiorPontuacao = -Infinity;
 
-    console.log("Lutas entre os personagens:\n");
+    console.log("--- COMEÇANDO O TORNEIO! ---\n");
 
     for (let i = 0; i < personagens.length; i++) {
         for (let j = 0; j < personagens.length; j++) {
@@ -174,35 +190,48 @@ function determinarVencedor(personagens) {
             const p2 = personagens[j];
 
             if (p1 === p2) {
-                console.log(`${p1.codinome} vs ${p2.codinome} ➜ Vencedor: O mesmo personagem`);
+                console.log(`\n**Batalha de exibição: ${p1.codinome} vs ele mesmo.**`);
+                console.log("O resultado é um empate, sem surpresas.");
             } else {
                 const score1 = p1.forca + p1.velocidade + p1.resistencia;
                 const score2 = p2.forca + p2.velocidade + p2.resistencia;
 
-                let ganhador;
+                let ganhador = "ninguém";
                 if (score1 > score2) {
                     ganhador = p1.codinome;
                 } else if (score2 > score1) {
                     ganhador = p2.codinome;
-                } else {
-                    ganhador = "Empate";
                 }
+                
+                console.log(`\n--- Batalha: ${p1.codinome} vs ${p2.codinome} ---`);
+                console.log("O ringue está pronto!");
+                console.log(`\n**Detalhes de ${p1.codinome}:**`);
+                console.log(p1.descricao());
+                console.log(`\n**Detalhes de ${p2.codinome}:**`);
+                console.log(p2.descricao());
 
-                console.log(`${p1.codinome} vs ${p2.codinome} ➜ Vencedor: ${ganhador}`);
+                if (ganhador === "ninguém") {
+                    console.log(`**Resultado da Luta:** Que luta acirrada! Foi um empate. Incrível!`);
+                } else {
+                    console.log(`**Resultado da Luta:** E o vencedor é... **${ganhador}**!`);
+                }
             }
         }
     }
     
     for (let i = 0; i < personagens.length; i++) {
         const total = personagens[i].forca + personagens[i].velocidade + personagens[i].resistencia;
-
         if (total > maiorPontuacao) {
             maiorPontuacao = total;
             vencedor = personagens[i];
         }
     }
 
-    console.log("\nCampeão Geral:\n" + vencedor.descricao());
+    console.log("\n--- FIM DO TORNEIO ---");
+    console.log(`\nApós todas as batalhas, o grande campeão geral é... **${vencedor.codinome}**!`);
+    console.log(`Com um poder total de ${maiorPontuacao}, ele dominou a competição.`);
+    console.log("\n**Ficha completa do Campeão:**");
+    console.log(vencedor.descricao());
 }
 
 determinarVencedor(lista);
